8c7d9d5b03b3b797850fa7b89a51167d3589707f
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-07-25 14:59:08 +0200

    added the functionality to the features rooms

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowswerRouter as Router } from 'react-router-dom';
 // import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
+import { RoomProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>
 );
 
