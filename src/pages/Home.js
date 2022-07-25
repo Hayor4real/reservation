821ad56fef6db73e987b1301643803be80ac86ac
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-07-25 12:50:11 +0200

    added some css properties

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
-
-export default function home() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
+const home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <>
+      <Hero>
+        <Banner
+          title='Affordable and Beautiful Rooms'
+          subtitle='deluxe rooms starting at €300'
+        >
+          <Link to='/rooms' className='btn-primary'>
+            Our reservation rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
+
+export default home;
